eefb1d8f69519021bace3dc8895445010b9c4598
swathi-sudeendra-rao <swathi.sudeendrarao001@stud.fh-dortmund.de>
2023-05-27 23:16:33 +0200

    cannot create submenus: reversing commit with commented code

--- app/src/main/java/com/ttd/dasasahitya/MainActivity.kt
+++ app/src/main/java/com/ttd/dasasahitya/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.ttd.dasasahitya
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ttd.dasasahitya.databinding.ActivityMainBinding
 import com.ttd.dasasahitya.fragments.MainFragment
@@ -15,4 +19,37 @@ class MainActivity : AppCompatActivity() {
         EncryptedSharedPreferenceUtil.saveSPBoolean(EncryptedSharedPreferenceUtil.isEnglish.value, true, DasaSahityaApp.applicationContext)
         supportFragmentManager.beginTransaction().replace(R.id.fragment, MainFragment()).commit()
     }
+
+    //@todo: add this code block once menu is finalized
+
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dotted_more_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.profile -> {
+                Toast.makeText(this,"Profile",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.events -> {
+                Toast.makeText(this,"Event",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.gallery -> {
+                Toast.makeText(this,"Gallery",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.website -> {
+                Toast.makeText(this,"Website",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.about -> {
+                Toast.makeText(this,"About",Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }*/
 }
